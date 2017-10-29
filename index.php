61494e1ce46189c863e106076119ba88362b2ba7
Sonnik73 <Sonnik73@gmail.com>
2017-10-30 00:49:35 +0400

    TestPicture 8

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
   </head>
   <body>
     <div id="root">Ну я уже начал че то делать)</div>
-<?php 
-   <img src="img/ dalmation.gif">; 
+//<?php 
+   <img src="img/ dalmation.gif">
    //<img src=" dalmation.gif">
-?>
+//?>
     <script src="js/react/react.js"></script>
     <script src="js/react/react-dom.js"></script>
     <script src="js/app.js"></script>
