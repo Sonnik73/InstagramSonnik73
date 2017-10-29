c43f02fbe44259e3df9172b619a587eda2dc658d
Sonnik73 <Sonnik73@gmail.com>
2017-10-30 01:24:34 +0400

    TestPicture 12

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
     <title>InstagrammSonnik73</title>
   </head>
   <body>
-    <div id="root">Ну я уже начал че то делать)
+    <div id="root">Ну я уже начал че то делать)</div>
 <!-- <?php 
 //<img src=/img/$dalmation.gif  border=0 width=15px height=15px>
    //<img src="img/ dalmation.gif">
    //<img src=" dalmation.gif">
 //echo $dalmation;
 ?> -->
+<br/>
 <img src="img/dalmation.gif" width="145" height="72" alt='картинка'/>
 
-</div>
+
     <script src="js/react/react.js"></script>
     <script src="js/react/react-dom.js"></script>
     <script src="js/app.js"></script>
