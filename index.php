a8be335af11439605c0cb1de8b2e978686c0c0af
Sonnik73 <Sonnik73@gmail.com>
2017-10-30 00:16:05 +0400

    Test picture

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 ﻿<!DOCTYPE html>
 <html>
   <head>
-    <title>React [RU] Tutorial</title>
+    <title>InstagrammSonnik73</title>
   </head>
   <body>
-    <div id="root">Привет, я #root</div>
-
+    <div id="root">Ну я уже начал че то делать)</div>
+		echo "<img src='http:⁄⁄lphp.ru/images/dalmation.gif'>"; 
     <script src="js/react/react.js"></script>
     <script src="js/react/react-dom.js"></script>
     <script src="js/app.js"></script>
