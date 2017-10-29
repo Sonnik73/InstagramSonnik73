8d6c34a8ccc989af3d098508cd30b7a35d2c701d
Sonnik73 <Sonnik73@gmail.com>
2017-10-30 00:48:06 +0400

    TestPicture 7

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
     <div id="root">Ну я уже начал че то делать)</div>
 <?php 
-   //echo "<img src="img/ dalmation.gif">"; 
+   <img src="img/ dalmation.gif">; 
    //<img src=" dalmation.gif">
 ?>
     <script src="js/react/react.js"></script>
